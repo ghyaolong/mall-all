a37cc472f2db54c82cd376439449b8c08e9ba1e3
yaochenglong <289911401@qq.com>
2019-05-27 23:17:12 +0800

    注册中心增加身份验证

---
+++ eureka-server/src/main/java/com/buildinglife/mall/server/center/eurekaserver/config/MyWebSecurityConfigurerAdapter.java
@@ -0,0 +1,23 @@
+package com.buildinglife.mall.server.center.eurekaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 后期开启服务认证配合oauth2.0的时候使用
+ */
+//@EnableWebSecurity
+//@Configuration
+public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
