4ea207619a904d74f23f18f34a6c5fce047db974
yaochenglong <289911401@qq.com>
2019-05-28 23:34:34 +0800

    新增其他模块

---
+++ member-service/src/main/java/com/buildinglifet/mall/memberservice/MemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildinglifet.mall.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServiceApplication.class, args);
+    }
+
+}
